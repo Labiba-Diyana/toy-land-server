739df54be87abaf7333d0d37a73c3b870c92e348
diyana <labibadiyana100@gmail.com>
2023-06-22 23:31:08 +0600

    update crud operations done

--- index.js
+++ index.js
@@ -72,18 +72,41 @@ async function run() {
 
         app.get('/toys/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
             const query = { _id: new ObjectId(id) };
             const result = await toyCollection.findOne(query);
             res.send(result);
         });
 
+        app.get('/toys/myToys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toyCollection.findOne(query);
+            res.send(result);
+        });
 
         app.post('/toys', async (req, res) => {
             const newToy = req.body;
             const result = await toyCollection.insertOne(newToy);
             res.send(result);
         });
+
+        app.put('/toys/myToys/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedToy = req.body;
+            const toy = {
+                $set: {
+                    price: updatedToy.price,
+                    quantity: updatedToy.quantity,
+                    detail: updatedToy.detail
+                }
+            }
+
+            const result = await toyCollection.updateOne(filter, toy, options);
+            res.send(result);
+        });
+
 
 
         app.delete('/toys/myToys/:id', async (req, res) => {
@@ -92,7 +115,7 @@ async function run() {
             const result = await toyCollection.deleteOne(query);
             res.send(result);
 
-        })
+        });
 
 
         // Send a ping to confirm a successful connection
